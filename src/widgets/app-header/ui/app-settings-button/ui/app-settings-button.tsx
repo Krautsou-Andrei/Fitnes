e5d39f2f495cdbd7e08ecf5b994a2fd959a5ca8f
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-11 00:43:48 +0300

    fix: change type to text AppSettingsButton

--- src/widgets/app-header/ui/app-settings-button/ui/app-settings-button.tsx
+++ src/widgets/app-header/ui/app-settings-button/ui/app-settings-button.tsx
@@ -3,11 +3,9 @@ import { SettingOutlined } from '@ant-design/icons';
 
 import { LayoutConfig } from '@shared/config';
 
-import styles from './app-setting-button.module.less';
-
 export function AppSettingsButton() {
     return (
-        <Button icon={<SettingOutlined />} size='middle' className={styles['button-settings']}>
+        <Button icon={<SettingOutlined />} size='middle' type='text'>
             {LayoutConfig.BUTTON_SETTINGS}
         </Button>
     );
