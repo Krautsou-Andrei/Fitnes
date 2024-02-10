b086200586e2c7aaa0c1def7e24e50ed20a391ab
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-10 10:25:00 +0300

    feat: add @neodx/svg
    BREAKING CHANGE: use SVG sprite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
+import svg from '@neodx/svg/vite';
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        svg({
+            group: true,
+            root: 'src/shared/ui/app-icon/assets',
+            output: 'public/sprite',
+            metadata: 'src/shared/ui/app-icon/sprite.h.ts',
+            resetColors: {
+                exclude: [/^layout/],
+                replaceUnknown: 'var(--icon-color)',
+            },
+        }),
+    ],
     server: {
         host: true,
         port: 3000,
