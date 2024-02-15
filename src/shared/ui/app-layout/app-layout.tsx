ee6068e2bc5c3a42e955305797e2534935cf77e2
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-15 21:45:37 +0300

    feat: add classnames from app-layout

--- src/shared/ui/app-layout/app-layout.tsx
+++ src/shared/ui/app-layout/app-layout.tsx
@@ -1,18 +1,20 @@
 import { ReactNode } from 'react';
-import { Layout } from 'antd';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+import clsn from 'classnames';
+import { Layout } from 'antd';
 
 import styles from './app-layout.module.less';
 
 type Props = {
+    className?: string;
     siderSlot?: ReactNode;
-    headerSlot: ReactNode;
+    headerSlot?: ReactNode;
     footerSlot?: ReactNode;
 };
 
-export function AppLayout({ siderSlot, headerSlot, footerSlot }: Props) {
+export function AppLayout({ className, siderSlot, headerSlot, footerSlot }: Props) {
     return (
-        <Layout className={styles['main-page']}>
+        <Layout className={clsn(styles['main-page'], className)}>
             {siderSlot}
             <Layout>
                 {headerSlot}
