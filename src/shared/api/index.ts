854b75d27782d6421b9dc9c38068d3f8d75c6e1d
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-17 17:59:06 +0300

    feat: add baseApi

---
+++ src/shared/api/index.ts
@@ -0,0 +1,5 @@
+import { baseApi } from './base-api';
+import { isFetchBaseQueryError } from './isFetchBaseQueryError';
+import { SESSION_TAG } from './tags';
+
+export { baseApi, SESSION_TAG, isFetchBaseQueryError };
