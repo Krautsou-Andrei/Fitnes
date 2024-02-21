7f70194437817dddd99d45968c730e7cf6eac07a
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-21 11:31:51 +0300

    feat: move logic to hook useRegisterForm

---
+++ src/features/register/lib/useRegisterForm.ts
@@ -0,0 +1,22 @@
+import { Form } from 'antd';
+
+import { registerThunk } from '../model/register';
+
+import { RequestRegisterBody } from '@entities/session';
+
+import { useAppDispatch } from '@shared/hooks';
+
+export function useRegisterForm() {
+    const [form] = Form.useForm();
+    const dispatch = useAppDispatch();
+
+    const onFinish = ({ email, password }: RequestRegisterBody) => {
+        dispatch(registerThunk({ email, password }))
+            .unwrap()
+            .catch((error: Error) => {
+                console.log('register', { type: 'server', message: error.message });
+            });
+    };
+
+    return { form, onFinish };
+}
