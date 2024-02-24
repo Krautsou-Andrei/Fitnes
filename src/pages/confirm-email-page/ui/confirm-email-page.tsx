47219f9b191021a2f419306202a5b5a1bc3345b0
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-24 09:32:11 +0300

    refactor: move logic ConfirmEmailPage to hook

--- src/pages/confirm-email-page/ui/confirm-email-page.tsx
+++ src/pages/confirm-email-page/ui/confirm-email-page.tsx
@@ -1,23 +1,25 @@
+import { useConfirmEmailPage } from '../lib/use-confirm-email-page';
+
 import { ConfirmForm } from '@features/confirm-email';
 import { AppResultCard } from '@features/result';
 import { confirmConfig } from '@features/confirm-email/config/confirm-config';
 
-import { SessionStorageConfig } from '@shared/config';
 import { TypePage } from '@shared/types/app';
 import { AppBackgroundBlur, AppGuestContent } from '@shared/ui';
 
 import styles from './confirm-email-page.module.less';
 
 export function ConfirmEmailPage() {
-    const sessionStorageEmail = sessionStorage.getItem(SessionStorageConfig.EMAIL);
-    const email = sessionStorageEmail ? sessionStorageEmail : '';
+    const { cardTitle, cardDescription } = useConfirmEmailPage({
+        classNames: [styles['select-text'], styles['select-text-block']],
+    });
 
     return (
         <AppBackgroundBlur>
             <AppGuestContent>
                 <AppResultCard
-                    title={confirmConfig[TypePage.CONFIRM_EMAIL].title}
-                    description={confirmConfig[TypePage.CONFIRM_EMAIL].description(email)}
+                    title={cardTitle}
+                    description={cardDescription}
                     icon={confirmConfig[TypePage.CONFIRM_EMAIL].icon}
                     className={styles['confirm-content']}
                     classNameIcon={styles[TypePage.CONFIRM_EMAIL]}
