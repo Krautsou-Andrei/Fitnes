6c65d9b57b54b80888a5dd01eedb7ec4f2f3eef8
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-02-26 22:20:11 +0300

    fix: delete sessionStorage email and password

--- src/features/register/model/register.ts
+++ src/features/register/model/register.ts
@@ -25,9 +25,9 @@ export const registerThunk = createAsyncThunk<void, RegisterParams, { state: Roo
         try {
             await dispatch(sessionApi.endpoints.register.initiate(body)).unwrap();
 
-            sessionStorage.setItem(SessionStorageConfig.EMAIL, '');
-            sessionStorage.setItem(SessionStorageConfig.PASSWORD, '');
-            
+            sessionStorage.removeItem(SessionStorageConfig.EMAIL);
+            sessionStorage.removeItem(SessionStorageConfig.PASSWORD);
+
             dispatch(push(PathConfig.RESULT_SUCCESS, { result: HistoryStateConfig.RESULT }));
         } catch (error: unknown | undefined) {
             sessionStorage.setItem(SessionStorageConfig.EMAIL, body.email);
