38e8ac8ff7fb34877893c7cb9f67a8bf323ee14c
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-03-01 13:33:08 +0300

    feat: add feedback in breadcrumbs

--- src/shared/config/layout-config.ts
+++ src/shared/config/layout-config.ts
@@ -14,6 +14,7 @@ export const enum LayoutConfig {
     ERROR_BOUNDARY_TITLE = 'Мы знаем о проблеме и работаем над ее устранением.',
     ERROR_BOUNDARY_SUBTITLE = 'Спасибо за терпение.',
     EXIT = 'Выход',
+    FEEDBACKS = 'Отзывы пользователей',
     HOME = 'Главная',
     INPUT_TEXT_EMAIL = 'e-mail:',
     INPUT_TEXT_PASSWORD = 'Пароль',
