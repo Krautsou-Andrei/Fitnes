1b777f7c32271ccbce703ed90e11ddd937c63752
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-03-08 23:44:27 +0300

    feat: add date-format-config

---
+++ src/shared/config/date-format-config.ts
@@ -0,0 +1,4 @@
+export const enum DateFormatConfig {
+    FORMAT_DD_MN_YYYY_DOT = 'DD.MM.YYYY',
+    FORMAT_YYYY_MM_DD_DASHED = 'YYYY-MM-DD',
+}
--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,5 +1,6 @@
 import { ConstantsMediaQuery } from './constants';
 import { DataTestIdConfig } from './data-test-id-config';
+import { DateFormatConfig } from './date-format-config';
 import { ErrorValidateConfig } from './error-validate-config';
 import { EventApiConfig } from './event-api-config';
 import { HistoryStateConfig } from './history-state-config';
@@ -12,6 +13,7 @@ import { SessionStorageConfig } from './session-storage-config';
 export {
     ConstantsMediaQuery,
     DataTestIdConfig,
+    DateFormatConfig,
     ErrorValidateConfig,
     EventApiConfig,
     HistoryStateConfig,
