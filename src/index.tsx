f3131a568e6bd671c6d4f7d34c1841e1ea572b0f
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-03-13 16:36:45 +0300

    fix: commit strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import 'normalize.css';
@@ -6,7 +6,7 @@ import App from '@app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <App />
-    </React.StrictMode>,
+    // </React.StrictMode>,
 );
