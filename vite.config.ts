1a79dffd0b20ab3b99cafee89f45c05e5498e4b4
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-03-24 14:08:09 +0300

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,41 +5,43 @@ import svg from '@neodx/svg/vite';
 
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    base: '/Krautsou-Andrei/',
-    plugins: [
-        react(),
-        svg({
-            group: true,
-            root: 'src/shared/ui/app-icon/assets',
-            output: 'public/sprite',
-            metadata: 'src/shared/ui/app-icon/sprite.h.ts',
-            resetColors: {
-                exclude: [/^layout/, /^result/, /^review/],
-                replaceUnknown: 'var(--icon-color)',
-            },
-        }),
-    ],
-    server: {
-        host: true,
-        port: 3000,
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true,
+export default defineConfig(({ command }) => {
+    return {
+        base: command === 'serve' ? '/' : 'ClevertecFrontendLab/Krautsou-Andrei',
+        plugins: [
+            react(),
+            svg({
+                group: true,
+                root: 'src/shared/ui/app-icon/assets',
+                output: 'public/sprite',
+                metadata: 'src/shared/ui/app-icon/sprite.h.ts',
+                resetColors: {
+                    exclude: [/^layout/, /^result/, /^review/],
+                    replaceUnknown: 'var(--icon-color)',
+                },
+            }),
+        ],
+        server: {
+            host: true,
+            port: 3000,
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                },
             },
         },
-    },
-    resolve: {
-        alias: {
-            '@public': path.resolve(__dirname, 'public'),
-            '@app': path.resolve(__dirname, 'src/app'),
-            '@entities': path.resolve(__dirname, 'src/entities'),
-            '@features': path.resolve(__dirname, 'src/features'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@widgets': path.resolve(__dirname, 'src/widgets'),
-            '@shared': path.resolve(__dirname, 'src/shared'),
+        resolve: {
+            alias: {
+                '@public': path.resolve(__dirname, 'public'),
+                '@app': path.resolve(__dirname, 'src/app'),
+                '@entities': path.resolve(__dirname, 'src/entities'),
+                '@features': path.resolve(__dirname, 'src/features'),
+                '@pages': path.resolve(__dirname, 'src/pages'),
+                '@widgets': path.resolve(__dirname, 'src/widgets'),
+                '@shared': path.resolve(__dirname, 'src/shared'),
+            },
         },
-    },
+    };
 });
