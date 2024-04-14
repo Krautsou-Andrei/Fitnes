686adb8962a9ea496e72653fbc4e119f71fd29ac
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-04-14 23:47:44 +0300

    feat: add info achievements card

---
+++ src/features/achievements/config/achievements-cards-config.ts
@@ -0,0 +1,20 @@
+import { AchievementsInfoConfig } from '../model/types';
+
+export const achievementsCardsConfig = [
+    {
+        title: AchievementsInfoConfig.ALL_VALUE,
+        description: 'Общая\n нагрузка, кг',
+    },
+    {
+        title: AchievementsInfoConfig.DAY_VALUE,
+        description: 'Нагрузка\n в день, кг',
+    },
+    {
+        title: AchievementsInfoConfig.REPLAYS,
+        description: 'Количество\n повторений, раз',
+    },
+    {
+        title: AchievementsInfoConfig.APPROACHES,
+        description: 'Подходы,\n раз',
+    },
+];
