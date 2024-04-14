72dbd5b7e12ae05016a7138b413ae8d538ac3eda
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2024-04-14 23:49:10 +0300

    feat: add exercises-info-achievements

--- src/features/achievements/config/achievements-config.ts
+++ src/features/achievements/config/achievements-config.ts
@@ -1,6 +1,9 @@
 export const enum AchievementsConfig {
     BUTTON_ALL = 'Все',
+    KG_TYPE = 'кг',
     TITLE_BUTTON_FILTER = 'Тип тренировки: ',
     TITLE_HISTOGRAM = 'Нагрузка, кг',
     TITLE_TABLE_ACIEVEMENTS = 'Средняя нагрузка\n по дням недели',
+    TITLE_TABLE_EXERCISES = 'Самые частые  упражнения\n по дням недели',
+    TITLE_TABLE_WEEK_EXERCISES = 'Самые частые  упражнения\n по дням недели',
 }
